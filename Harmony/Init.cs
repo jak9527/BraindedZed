37888a29244679ce47eabf45d5616ca31881d22e
Jacob Karvelis <jak9527@rit.edu>
2025-04-14 17:30:37 -0400

    Add project files.

---
+++ Harmony/Init.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+namespace Harmony
+{
+    public class SampleProject : IModApi
+    {
+        public void InitMod(Mod _modInstance)
+        {
+            var harmony = new HarmonyLib.Harmony(_modInstance.Name);
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
